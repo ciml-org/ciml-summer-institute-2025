746db7fd00d0e833c18d0e8a2dbf49bbd336126d
Marty Kandes <mkandes@sdsc.edu>
2025-06-24 09:36:46 -0700

    Update run-tf2-train-cnn-cifar-compute.sh

--- 2.6_software_containers_for_scientific_and_hpc/run-tf2-train-cnn-cifar-compute.sh
+++ 2.6_software_containers_for_scientific_and_hpc/run-tf2-train-cnn-cifar-compute.sh
@@ -16,12 +16,12 @@ declare -xr LUSTRE_PROJECT_DIR="/expanse/lustre/projects/${SLURM_ACCOUNT}/${USER
 declare -xr LUSTRE_SCRATCH_DIR="/expanse/lustre/scratch/mkandes/temp_project"
 declare -xr LOCAL_SCRATCH_DIR="/scratch/${USER}/job_${SLURM_JOB_ID}"
 
-declare -xr SINGULARITY_MODULE='singularitypro/3.9'
-declare -xr SINGULARITY_CONTAINER_DIR="${LUSTRE_SCRATCH_DIR}"
+declare -xr SINGULARITY_MODULE='singularitypro/3.11'
+declare -xr SINGULARITY_CONTAINER_DIR='/cm/shared/apps/containers/singularity/tensorflow'
 
 module purge
 module load "${SINGULARITY_MODULE}"
 module list
 printenv
 
-time -p singularity exec "${SINGULARITY_CONTAINER_DIR}/tensorflow-2.8.2-ubuntu-20.04-cuda-11.2-mlnx-ofed-4.9-4.1.7.0-openmpi-4.1.3.sif" python3 -u tf2-train-cnn-cifar.py --classes 10 --precision fp32 --epochs 42 --batch_size 256
+time -p singularity exec "${SINGULARITY_CONTAINER_DIR}/tensorflow-2.8.3-ubuntu-20.04-cuda-11.2-mlnx-ofed-4.9-4.1.7.0-openmpi-4.1.3-20221008.sif" python3 -u tf2-train-cnn-cifar.py --classes 10 --precision fp32 --epochs 42 --batch_size 256
